a7b4fd3965e97262032f8f9cd51448f91a76687a
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-06 22:20:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon("./lib/emoticons.yml", emoticon_eng)
   emoticon_library.each do |emotion, emoticons|
     emoticons.each do |language, emoticon|
       if emoticon == emoticon_eng
-        return emoticon_library[emotion][:japanese]
+        emoticon_library[emotion][:japanese]
       end
     end
   end
