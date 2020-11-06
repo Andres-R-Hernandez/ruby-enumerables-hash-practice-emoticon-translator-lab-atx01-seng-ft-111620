afb0de671ac760498c6df6d9642eb8f59273f144
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-06 22:04:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "yaml"
 def load_library
   # code goes here
   emoticon_library = YAML.load_file('lib/emoticons.yml')
-  emoticon_library.each_with_object({}) do |(emotion, emoticons), emoticons_clean|
+  final_result = emoticon_library.each_with_object({}) do |(emotion, emoticons), emoticons_clean|
       emoticons_clean[emotion] = {}
       emoticons_clean[emotion][:english] = emoticons[0]
       emoticons_clean[emotion][:japanese] = emoticons[1]
